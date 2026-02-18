69cbf11047f367a7d24bc8f6704fca639df8b6fe
66010914079 <66010914079@msu.ac.th>
2026-02-18 09:16:33 +0700

    supattra

--- j/connectdb.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "", "4063db");
-if (!$conn) { 
-    die("Connection failed: " . mysqli_connect_error()); 
-}
-mysqli_query($conn, "SET NAMES utf8"); // ตั้งค่ารองรับภาษาไทย
-?>
